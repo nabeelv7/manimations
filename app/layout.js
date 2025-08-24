60a33fdf2a5feaca7c265d3722abed1ed7ca00ad
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-08-24 17:45:27 +0500

    reinit

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const dmSans = Bricolage_Grotesque({
   subsets: ["latin"],
 });
 
@@ -20,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${dmSans.className} antialiased`}
       >
         {children}
       </body>
